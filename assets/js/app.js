2e47d5e7f678eee80920653811c6fba16c003e36
billy-king <billy-king@vm-0.billy-king.koding.kd.io>
2013-12-01 11:00:56 +0000

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,116 @@
+/**
+ * app.js
+ *
+ * This file contains some conventional defaults for working with Socket.io + Sails.
+ * It is designed to get you up and running fast, but is by no means anything special.
+ *
+ * Feel free to change none, some, or ALL of this file to fit your needs!
+ */
+
+
+(function (io) {
+
+  // as soon as this file is loaded, connect automatically, 
+  var socket = io.connect();
+  if (typeof console !== 'undefined') {
+    log('Connecting to Sails.js...');
+  }
+
+  socket.on('connect', function socketConnected() {
+
+    // Listen for Comet messages from Sails
+    socket.on('message', function messageReceived(message) {
+
+      ///////////////////////////////////////////////////////////
+      // Replace the following with your own custom logic
+      // to run when a new message arrives from the Sails.js
+      // server.
+      ///////////////////////////////////////////////////////////
+      log('New comet message received :: ', message);
+      //////////////////////////////////////////////////////
+
+    });
+
+
+    ///////////////////////////////////////////////////////////
+    // Here's where you'll want to add any custom logic for
+    // when the browser establishes its socket connection to 
+    // the Sails.js server.
+    ///////////////////////////////////////////////////////////
+    log(
+        'Socket is now connected and globally accessible as `socket`.\n' + 
+        'e.g. to send a GET request to Sails, try \n' + 
+        '`socket.get("/", function (response) ' +
+        '{ console.log(response); })`'
+    );
+    ///////////////////////////////////////////////////////////
+
+
+  });
+
+
+  // Expose connected `socket` instance globally so that it's easy
+  // to experiment with from the browser console while prototyping.
+  window.socket = socket;
+
+
+  // Simple log function to keep the example simple
+  function log () {
+    if (typeof console !== 'undefined') {
+      console.log.apply(console, arguments);
+    }
+  }
+  
+
+})(
+
+  // In case you're wrapping socket.io to prevent pollution of the global namespace,
+  // you can replace `window.io` with your own `io` here:
+  window.io
+
+);
+
+function rootController($scope , $http){
+      socket.on('connect', function socketConnected() {
+        socket.on('message', function messageReceived(message) {
+            console.log(message);
+            if (message.verb === "create") {
+                $scope.todos.push(message.data);
+                $scope.$apply();
+            }
+            
+            if (message.verb === "destroy") {
+                readEvent();
+            }
+        });
+    });
+    socket.get("/todo", function (response) { console.log(response); });
+  
+    function readEvent () {
+        socket.get("/todo", function (data) { 
+            $scope.todos = data;
+            $scope.$apply();
+        });
+    }
+    
+    readEvent();
+    
+    $scope.addEvent = function () {
+        socket.post("/todo/create", {
+            title: $scope.title,
+            speak: $scope.speak
+        }, function (message) {
+            console.log(message);
+            // $scope.todos.push(message.data);
+            $scope.speak = "";
+            $scope.$apply();
+            
+        })
+    };
+     $scope.destroyEvent = function (id) {
+        socket.get("/todo/destroy/" + id, function (message) {
+            // console.log(data);
+            // readEvent();
+        });
+    };
+}
